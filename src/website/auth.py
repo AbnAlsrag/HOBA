ce33e38c0ab08db816fb2ccf5bb7fab8dcfe6d59
Abn Alsrag <85142438+AbnAlsrag@users.noreply.github.com>
2023-10-21 12:47:39 +0300

    I have done some shit

--- src/website/auth.py
+++ src/website/auth.py
@@ -1,15 +1,32 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint("auth", __name__)
 
-@auth.route("/login")
+@auth.route("/login", methods=["GET", "POST"])
 def login():
-    return "<p>LOGIN</p>"
+    data = request.form
+    print(data)
+    return render_template("login.html")
 
 @auth.route("/logout")
 def logout():
-    return "<p>LOGOUT</p>"
+    return render_template("logout.html")
 
-@auth.route("/sign-up")
+@auth.route("/sign-up", methods=["GET", "POST"])
 def sign_up():
-    return "<p>SIGN UP</p>"
+    if request.method == "POST":
+        email = request.form.get("email")
+        name = request.form.get("name")
+        password = request.form.get("password-1")
+        password_confirm = request.form.get("password-2")
+
+        if len(email) < 4:
+            flash("Your email seems to be wrong type it again", category="error")
+        elif password != password_confirm:
+            flash("Your password doesn't match the confirmation password", category="error")
+        elif len(password) < 4:
+            flash("Your password it to short", category="error")
+        else:
+            flash("Account created!", category="success")
+
+    return render_template("sign-up.html")
